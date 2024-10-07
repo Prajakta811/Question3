57bf5d65d216f092a5b256b3a032218b43abb9c9
Prajakta <prajaktas811@gmail.com>
2024-10-07 10:15:07 +0530

    Modified Ouput in One Line

--- Question6/index.js
+++ Question6/index.js
@@ -1,9 +1,10 @@
 let arr=[2,45,67,3,9,54,128,100,102346]
-
+let EvenNumber;
 for(let i=1;i<arr.length;i++)
 {
     if(arr[i]%2===0)
     {
-        console.log("Even Numbers Are"+ arr[i]);
+        EvenNumber=a[i]; 
     }
+    console.log("Even Numbers Are "+ EvenNumber);
 }
