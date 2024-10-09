3729edc5ff726f8e18c27fbd8b0337b17c15dd35
Prajakta <prajaktas811@gmail.com>
2024-10-09 15:38:17 +0530

    Written Code to Check valid  Age for Marriage

---
+++ index.js
@@ -0,0 +1,11 @@
+//Code to check valid marriage age:
+let girlAge= prompt("Enter Female Age");
+let boyAge=prompt("Enter Male Age");
+if((girlAge>18)&&(boyAge>21))
+{
+    console.log(" Eligible for Marriage");
+}
+else
+{
+    console.log("Not Eligible");
+}
